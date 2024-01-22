6d35cc4051d28b2cf54d17d179ebe688754b9394
Deneir Uy <deneiruy@gmail.com>
2024-01-22 21:44:47 +0800

    Add migrations

--- components/base_templ.go
+++ components/base_templ.go
@@ -27,7 +27,7 @@ func base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var2 := `Hello, world!`
+		templ_7745c5c3_Var2 := `Tymsa`
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
